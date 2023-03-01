ce55e2b665c307ee76315b7c32009afb2192c9dd
Mehmet Polat <mirmehmetpolat@gmail.com>
2023-03-01 12:13:15 +0300

    oil filter methods have been changed for CITROEN-CHEVROLET-DACIA

--- periyodikbakimci/Form1.Designer.cs
+++ periyodikbakimci/Form1.Designer.cs
@@ -165,7 +165,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.ClientSize = new System.Drawing.Size(1424, 786);
+            this.ClientSize = new System.Drawing.Size(1409, 786);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.TxtMevcutMrk);
